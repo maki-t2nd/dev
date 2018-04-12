614ce5887faa4220915d332402cedae38b024ab2
牧田 友和 <maki_t@maki-o.net>
2018-04-12 20:26:02 +0900

    test

--- sw.js
+++ sw.js
@@ -25,7 +25,28 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         }
-        // return fetch(event.request);
+
+        
+        return fetch(event.request).then(
+          function(response) {
+            // レスポンスが正しいかをチェック
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // 重要：レスポンスを clone する。レスポンスは Stream で
+            // ブラウザ用とキャッシュ用の2回必要。なので clone して
+            // 2つの Stream があるようにする
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
       }
     )
   );
